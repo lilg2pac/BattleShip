fcf86a929f4c3d24c83f3bf131e1f6b4d76886c0
Kuzmin <RMKuzmin@kiev.luxoft.com>
2017-06-21 18:27:21 +0300

    initial commit

---
+++ src/main/java/battleship/enums/ShipType.java
@@ -0,0 +1,13 @@
+package battleship.enums;
+
+
+public enum ShipType {
+
+	ONE_DECK,
+	
+	TWO_DECK,
+	
+	THREE_DECK,
+	
+	FOUR_DECK
+}
---
+++ src/main/java/battleship/model/ShipPosition.java
@@ -0,0 +1,24 @@
+package battleship.model;
+
+public class ShipPosition {
+	
+	private int x;
+	
+	private int y;
+
+	public void setX(int x) {
+		this.x = x;
+	}
+	
+	public int getX() {
+		return x;
+	}
+	
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	public int getY() {
+		return y;
+	}
+}
